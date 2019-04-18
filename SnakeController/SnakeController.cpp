568fa77ddc11a887028ad86026ea24667700816f
Lukasz Los <luk@luk.com>
2019-04-18 19:11:31 +0200

    Zadanie 2

--- SnakeController/SnakeController.cpp
+++ SnakeController/SnakeController.cpp
@@ -215,23 +215,32 @@ Controller::Segment Controller::getNewHead() const
 
 void Controller::receive(std::unique_ptr<Event> e)
 {
-    try {
-        handleTimePassed(*dynamic_cast<EventT<TimeoutInd> const&>(*e));
-    } catch (std::bad_cast&) {
-        try {
-            handleDirectionChange(*dynamic_cast<EventT<DirectionInd> const&>(*e));
-        } catch (std::bad_cast&) {
-            try {
-                handleFoodPositionChange(*dynamic_cast<EventT<FoodInd> const&>(*e));
-            } catch (std::bad_cast&) {
-                try {
-                    handleNewFood(*dynamic_cast<EventT<FoodResp> const&>(*e));
-                } catch (std::bad_cast&) {
-                    throw UnexpectedEventException();
-                }
-            }
-        }
-    }
+       switch (e->getMessageId())
+        {
+        case 0x20:
+        handleTimePassed(*static_cast<EventT<TimeoutInd> const&> (*e));
+        break;
+
+       case 0x10:
+       handleDirectionChange(*static_cast<EventT<DirectionInd> const&> (*e));
+       break;
+
+       case 0x40:
+       handleFoodPositionChange(*static_cast<EventT<FoodInd> const&> (*e));
+       break;
+
+       case 0x42:
+       handleNewFood(*static_cast<EventT<FoodResp> const&> (*e));
+       break;
+
+       default:
+       break;
+       }
+
 }
+
+
+
+
 
 } // namespace Snake
